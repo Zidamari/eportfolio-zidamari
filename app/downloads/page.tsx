29743bf783f435dd6fb0fe22657dc03af564ef15
Zidamari <mohdzidane@outlook.com>
2024-06-01 01:30:57 +0800

    Update page.tsx

--- app/downloads/page.tsx
+++ app/downloads/page.tsx
@@ -1,34 +1,63 @@
-import React from 'react';
+"use client";
+import { Github, Mail, Linkedin } from "lucide-react";
+import Link from "next/link";
+import { Navigation } from "../components/nav";
+import { Card } from "../components/card";
 
-const Placeholder: React.FC = () => {
-  return (
-    <div style={styles.container}>
-      <h1 style={styles.header}>Page Under Construction</h1>
-      <p style={styles.paragraph}>
-        This page is currently under construction. Please check back later for updates.
-      </p>
-    </div>
-  );
-};
+const socials = [
+	{
+		icon: <Linkedin size={20} />,
+		href: "www.linkedin.com/in/zidanemarican",
+		label: "LinkedIn",
+		handle: "zidanemarican",
+	},
+	{
+		icon: <Mail size={20} />,
+		href: "mohdzidane@outlook.com",
+		label: "Email",
+		handle: "mohdzidane@outlook.com",
+	},
+	{
+		icon: <Github size={20} />,
+		href: "https://github.com/Zidamari",
+		label: "Github",
+		handle: "Zidamari",
+	},
+];
 
-const styles: { [key: string]: React.CSSProperties } = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    textAlign: 'center',
-    fontFamily: 'Arial, sans-serif',
-  },
-  header: {
-    fontSize: '2.5em',
-    marginBottom: '20px',
-  },
-  paragraph: {
-    fontSize: '1.2em',
-    lineHeight: '1.6',
-  },
-};
-
-export default Placeholder;
+export default function Example() {
+	return (
+		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+			<Navigation />
+			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+					{socials.map((s) => (
+						<Card>
+							<Link
+								href={s.href}
+								target="_blank"
+								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+							>
+								<span
+									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+									aria-hidden="true"
+								/>
+								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+									{s.icon}
+								</span>{" "}
+								<div className="z-10 flex flex-col items-center">
+									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
+										{s.handle}
+									</span>
+									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+										{s.label}
+									</span>
+								</div>
+							</Link>
+						</Card>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
