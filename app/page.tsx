cd090f715923b73fd9f92013e237d532b7e036c8
Zidamari <mohdzidane@outlook.com>
2024-06-03 13:49:04 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm a Robotics Student from the Singapore Institute of Technology and this is my ePortfolio. Feel free to explore!\n
+          I'm a Robotics Student from the Singapore Institute of Technology and this is my ePortfolio. Feel free to explore!<br />
           Template provided by Vercel, made by Chronoark
         </h2>
       </div>
