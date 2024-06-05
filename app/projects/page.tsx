76762cdc2efe22655f27f35de008e843bd363860
Zidamari <mohdzidane@outlook.com>
2024-06-05 20:40:02 +0800

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -32,7 +32,7 @@ export default async function ProjectsPage() {
             Projects
           </h2>
           <p className="mt-4 text-zinc-400">
-            Some of the projects are from work and some are on my own time.
+            The projects listed are varies from work to personal achievements.
           </p>
         </div>
         <div className="w-full h-px bg-zinc-800" />
