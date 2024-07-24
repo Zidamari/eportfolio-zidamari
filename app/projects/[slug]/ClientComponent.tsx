44a47fd50349e53a86f15108d56a36257dac80f6
Zidamari <mohdzidane@outlook.com>
2024-07-25 00:38:28 +0800

    plese left

--- app/projects/[slug]/ClientComponent.tsx
+++ app/projects/[slug]/ClientComponent.tsx
@@ -26,9 +26,9 @@ const ClientComponent = ({ slug, project, views }: Props) => {
   }, []);
 
   return (
-    <div className="relative w-screen overflow-hidden">
+    <div className="relative w-screen h-screen bg-white overflow-hidden">
       <div className="flex flex-row items-start justify-between p-8">
-        <div className="w-1/3 h-[400px] sticky top-8 bg-white p-8 rounded-lg shadow-lg">
+        <div className="w-1/3 h-[400px] sticky top-8 bg-white p-8 rounded-lg shadow-lg" style={{ marginLeft: '-50px', marginTop: '-50px' }}>
           <SEP2Model className="w-full h-full" />
         </div>
         <div className="w-2/3 bg-white p-8 rounded-lg shadow-lg ml-8">
@@ -40,4 +40,5 @@ const ClientComponent = ({ slug, project, views }: Props) => {
     </div>
   );
 };
+
 export default ClientComponent;
