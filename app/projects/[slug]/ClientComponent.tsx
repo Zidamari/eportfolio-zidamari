82b1fca9e142430b71d71d2d155e869eff0e6333
Zidamari <mohdzidane@outlook.com>
2024-07-24 21:31:09 +0800

    plese

--- app/projects/[slug]/ClientComponent.tsx
+++ app/projects/[slug]/ClientComponent.tsx
@@ -31,7 +31,7 @@ const ClientComponent = ({ slug, project, views }: Props) => {
         <div className="w-1/3 h-96 sticky top-8 bg-white p-8 rounded-lg shadow-lg">
           <SEP2Model className="w-full h-full" />
         </div>
-        <div className="w-2/3 bg-white p-8 rounded-lg shadow-lg ml-8">
+        <div className="w-2/3 bg-white rounded-lg shadow-lg ml-8">
           <article className="prose prose-zinc prose-quoteless">
             <Mdx code={project.body.code} />
           </article>
