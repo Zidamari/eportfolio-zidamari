754bdfe5a53e171802479bc636edcf583caee349
Zidamari <mohdzidane@outlook.com>
2024-07-25 00:12:37 +0800

    plese left

--- app/projects/[slug]/ClientComponent.tsx
+++ app/projects/[slug]/ClientComponent.tsx
@@ -14,23 +14,6 @@ type Props = {
   views: any;
 };
 
-function SEP2Model() {
-  const ref = useRef<Mesh>(null!);
-  useFrame(() => {
-    ref.current.rotation.y += 0.01;
-  });
-  return (
-    <mesh ref={ref}>
-      <sphereGeometry args={[1, 60, 60]} />
-      <meshBasicMaterial color="hotpink" />
-    </mesh>
-  );
-}
-
-const AmbientLightComponent = () => {
-  return <primitive object={new AmbientLight()} />;
-};
-
 const ClientComponent = ({ slug, project, views }: Props) => {
   const [scrollY, setScrollY] = useState(0);
 
@@ -38,25 +21,19 @@ const ClientComponent = ({ slug, project, views }: Props) => {
     const handleScroll = () => {
       setScrollY(window.scrollY);
     };
-    window.addEventListener('scroll', handleScroll);
+
+    window.addEventListener("scroll", handleScroll);
+
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <div className="relative w-screen h-screen bg-white overflow-hidden">
+    <div className="relative w-screen overflow-hidden">
       <div className="flex flex-row items-start justify-between p-8">
-        <div
-          className="absolute top-0 left-0 w-[300px] h-[300px] bg-white p-8 rounded-lg shadow-lg"
-          style={{ marginLeft: '20px', marginTop: '20px' }}
-        >
-          <Canvas>
-            <AmbientLightComponent />
-            <PointLight position={[10, 10, 10]} />
-            <OrbitControls />
-            <SEP2Model scale={0.5} />
-          </Canvas>
+        <div className="w-1/3 h-[400px] sticky top-8 bg-white p-8 rounded-lg shadow-lg">
+          <SEP2Model className="w-full h-full" />
         </div>
         <div className="w-2/3 bg-white p-8 rounded-lg shadow-lg ml-8">
           <article className="prose prose-zinc prose-quoteless">
