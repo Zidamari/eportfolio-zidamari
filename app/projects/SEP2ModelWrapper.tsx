555b4730fc26f2d9584d2ba27804e737bd944296
Zidamari <mohdzidane@outlook.com>
2024-07-24 12:36:01 +0800

    Create SEP2ModelWrapper.tsx

---
+++ app/projects/SEP2ModelWrapper.tsx
@@ -0,0 +1,11 @@
+// app/projects/SEP2ModelWrapper.tsx
+"use client"; // Mark this file as a Client Component
+
+import React from 'react';
+import SEP2Model from './SEP2Model';
+
+const SEP2ModelWrapper = () => {
+  return <SEP2Model />;
+};
+
+export default SEP2ModelWrapper;
