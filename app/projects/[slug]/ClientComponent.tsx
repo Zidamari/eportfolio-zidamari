534cc21db09ef1e1b5cc6a6d3923b2ce887992e8
Zidamari <mohdzidane@outlook.com>
2024-07-25 01:14:10 +0800

    plese left

--- app/projects/[slug]/ClientComponent.tsx
+++ app/projects/[slug]/ClientComponent.tsx
@@ -26,11 +26,11 @@ const ClientComponent = ({ slug, project, views }: Props) => {
   }, []);
 
   return (
-    <div className="relative w-screen overflow-hidden">
-      <div className="flex flex-row items-start justify-between p-8">
-        <div className="w-1/3 h-[400px] sticky top-8 bg-white p-8 rounded-lg shadow-lg">
-          <SEP2Model className="w-full h-full" />
-        </div>
+    <div className="relative w-screen h-screen bg-white overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full" style={{ zIndex: -1 }}>
+        <SEP2Model className="w-full h-full" />
+      </div>
+      <div className="relative flex flex-row items-start justify-between p-8" style={{ zIndex: 1 }}>
         <div className="w-2/3 bg-white p-8 rounded-lg shadow-lg ml-8">
           <article className="prose prose-zinc prose-quoteless">
             <Mdx code={project.body.code} />
@@ -40,4 +40,5 @@ const ClientComponent = ({ slug, project, views }: Props) => {
     </div>
   );
 };
+
 export default ClientComponent;
