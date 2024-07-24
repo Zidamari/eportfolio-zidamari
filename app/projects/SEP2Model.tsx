d18c1169d1ef509625211a5ff8625102495fa355
Zidamari <mohdzidane@outlook.com>
2024-07-25 01:24:10 +0800

    plese left

--- app/projects/SEP2Model.tsx
+++ app/projects/SEP2Model.tsx
@@ -3,14 +3,18 @@ import { Canvas, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "@react-three/drei";
 
+type SEP2ModelProps = {
+  className?: string;
+};
+
 const Model = () => {
   const gltf = useLoader(GLTFLoader, "/models/SEP2Model.gltf");
   return <primitive object={gltf.scene} scale={0.5} />;
 };
 
-const SEP2Model = () => {
+const SEP2Model: React.FC<SEP2ModelProps> = ({ className }) => {
   return (
-    <Canvas className="w-full h-full">
+    <Canvas className={className}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} intensity={1} />
       <Model />
