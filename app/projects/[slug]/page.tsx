241d9c3bfb1c195e0800500686125caf03ba3997
Zidamari <mohdzidane@outlook.com>
2024-07-24 19:56:45 +0800

    plese

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -3,8 +3,8 @@ import { allProjects } from "contentlayer/generated";
 import { Mdx } from "@/app/components/mdx";
 import { Header } from "./header";
 import "./mdx.css";
+import { ReportView } from "./view";
 import { Redis } from "@upstash/redis";
-import ClientComponent from "./ClientComponent";
 
 export const revalidate = 60;
 
@@ -38,7 +38,11 @@ export default async function PostPage({ params }: Props) {
   return (
     <div className="bg-zinc-50 min-h-screen">
       <Header project={project} views={views} />
-      <ClientComponent slug={slug} project={project} views={views} />
+      <ReportView slug={project.slug} />
+
+      <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
+        <Mdx code={project.body.code} />
+      </article>
     </div>
   );
 }
