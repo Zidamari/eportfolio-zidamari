3ec5fc31794656954bf00e5759b25c8385d04d98
Zidamari <mohdzidane@outlook.com>
2024-07-25 00:48:18 +0800

    plese left

--- app/projects/[slug]/ClientComponent.tsx
+++ app/projects/[slug]/ClientComponent.tsx
@@ -28,12 +28,10 @@ const ClientComponent = ({ slug, project, views }: Props) => {
   return (
     <div className="relative w-screen h-screen bg-white overflow-hidden">
       <div className="flex flex-row items-start justify-between p-8">
-        <div className="relative w-1/3 h-96 bg-white p-8 rounded-lg shadow-lg" style={{ zIndex: 1 }}>
-          <div className="absolute top-[-50px] left-[-50px] w-full h-full" style={{ zIndex: -1 }}>
-            <SEP2Model className="w-full h-full" />
-          </div>
+        <div className="relative w-1/3 h-96 bg-white p-8 rounded-lg shadow-lg" style={{ zIndex: -1 }}>
+          <SEP2Model className="w-full h-full" />
         </div>
-        <div className="relative w-2/3 bg-white p-8 rounded-lg shadow-lg ml-8" style={{ zIndex: 2 }}>
+        <div className="relative w-2/3 bg-white p-8 rounded-lg shadow-lg ml-8" style={{ zIndex: 1 }}>
           <article className="prose prose-zinc prose-quoteless">
             <Mdx code={project.body.code} />
           </article>
