b567f0ba9e001440ebeb572d78853385119692e8
Zidamari <mohdzidane@outlook.com>
2024-07-25 00:55:03 +0800

    plese left

--- app/projects/[slug]/ClientComponent.tsx
+++ app/projects/[slug]/ClientComponent.tsx
@@ -27,19 +27,17 @@ const ClientComponent = ({ slug, project, views }: Props) => {
 
   return (
     <div className="relative w-screen h-screen bg-white overflow-hidden">
-      <div className="relative w-full h-full">
-        <div className="absolute top-0 left-0 w-full h-full" style={{ zIndex: -1 }}>
-          <SEP2Model className="w-full h-full" />
+      <div className="absolute top-0 left-0 w-full h-full" style={{ zIndex: -1 }}>
+        <SEP2Model className="w-full h-full" />
+      </div>
+      <div className="relative flex flex-row items-start justify-between p-8" style={{ zIndex: 1 }}>
+        <div className="w-1/3 h-96 sticky top-8 bg-white p-8 rounded-lg shadow-lg">
+          {/* Any additional content */}
         </div>
-        <div className="relative flex flex-row items-start justify-between p-8" style={{ zIndex: 1 }}>
-          <div className="w-1/3 h-96 bg-white p-8 rounded-lg shadow-lg">
-            {/* Any additional content */}
-          </div>
-          <div className="w-2/3 bg-white p-8 rounded-lg shadow-lg ml-8">
-            <article className="prose prose-zinc prose-quoteless">
-              <Mdx code={project.body.code} />
-            </article>
-          </div>
+        <div className="w-2/3 bg-white p-8 rounded-lg shadow-lg ml-8">
+          <article className="prose prose-zinc prose-quoteless">
+            <Mdx code={project.body.code} />
+          </article>
         </div>
       </div>
     </div>
