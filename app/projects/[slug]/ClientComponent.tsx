1012935f473a93459a83e761df23f1a877c4a95d
Zidamari <mohdzidane@outlook.com>
2024-07-24 19:42:24 +0800

    ci

--- app/projects/[slug]/ClientComponent.tsx
+++ app/projects/[slug]/ClientComponent.tsx
@@ -29,7 +29,7 @@ const ClientComponent = ({ slug, project, views }: Props) => {
     <div className="relative w-screen h-screen bg-white overflow-hidden">
       <div className="flex flex-row items-start justify-between p-8">
         <div className="w-1/3 h-96 sticky top-8 bg-white p-8 rounded-lg shadow-lg">
-          <SEP2Model className="model-background" />
+          <SEP2Model className="w-full h-full" />
         </div>
         <div className="w-2/3 bg-white p-8 rounded-lg shadow-lg ml-8">
           <article className="prose prose-zinc prose-quoteless">
