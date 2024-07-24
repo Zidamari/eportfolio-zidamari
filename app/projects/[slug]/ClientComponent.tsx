68538af4cbcc68773f832b9cd1aac015b35f2475
Zidamari <mohdzidane@outlook.com>
2024-07-24 22:54:57 +0800

    plese man

--- app/projects/[slug]/ClientComponent.tsx
+++ app/projects/[slug]/ClientComponent.tsx
@@ -1,15 +1,3 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import SEP2Model from "@/app/projects/SEP2Model";
-import { Mdx } from "@/app/components/mdx"; // Import the Mdx component
-
-type Props = {
-  slug: string;
-  project: any;
-  views: number;
-};
-
 const ClientComponent = ({ slug, project, views }: Props) => {
   const [scrollY, setScrollY] = useState(0);
 
@@ -28,10 +16,10 @@ const ClientComponent = ({ slug, project, views }: Props) => {
   return (
     <div className="relative w-screen h-screen bg-white overflow-hidden">
       <div className="flex flex-row items-start justify-between p-8">
-        <div className="w-1/3 h-96 sticky top-8 bg-white p-8 rounded-lg shadow-lg">
+        <div className="w-1/3 h-96 sticky top-8 bg-white p-8 rounded-lg shadow-lg z-10">
           <SEP2Model className="w-full h-full" />
         </div>
-        <div className="w-2/3 bg-white p-8 rounded-lg shadow-lg ml-8">
+        <div className="w-2/3 bg-white p-8 rounded-lg shadow-lg ml-8 z-20">
           <article className="prose prose-zinc prose-quoteless">
             <Mdx code={project.body.code} />
           </article>
